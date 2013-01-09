06f216e5d4f1803f8ef671e5534ebb219b696ac3
Max Hauser <max@zweiundvierzig.at>
2013-01-09 17:05:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+A semantic version library for .Net
+===================================
+
+This class library implements the SemVersion class, that
+complies to v2.0.0-rc.1 of the spec from http://semver.org.
+
+Parsing 
+--------
+
+     var version = SemVersion.Parse("1.1.0-rc.1+nightly.2345");
+
+Comparing
+---------
+
+     if(version >= "1.0")
+         Console.WriteLine("released version {0}!", version)
---
+++ Semver/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("A semantic version implementation.")]
+[assembly: AssemblyCompany("Max Hauser")]
+[assembly: AssemblyProduct("Semver")]
+[assembly: AssemblyCopyright("Copyright © 2013 Max Hauser")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e208ca67-5b59-45d9-a29a-7f30137d3beb")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
