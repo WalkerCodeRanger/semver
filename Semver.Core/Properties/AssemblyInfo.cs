45c6882191851ff97a8cbd60794ba26e440eb527
Adam <a_pawsey@hotmail.com>
2016-06-23 09:42:27 +0200

    Fix: guid attribute (somehow slipped previous compile)

--- Semver.Core/Properties/AssemblyInfo.cs
+++ Semver.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -14,6 +13,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7bee814b-cff1-42dd-94f4-c23f4318a45e")]
