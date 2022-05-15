fcca14434be1b06d3615832cd8ec8ee2f9573dd4
Jeff Walker <Jeff@WalkerCodeRanger.com>
2022-05-15 11:50:46 -0700

    docs for ISemVersionComparer

--- Semver/Comparers/ISemVersionComparer.cs
+++ Semver/Comparers/ISemVersionComparer.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 
 namespace Semver.Comparers
 {
+    /// <summary>
+    /// An interface that combines equality and order comparison for the <see cref="SemVersion"/>
+    /// class.
+    /// </summary>
+    /// <remarks>
+    /// This interface provides a type for the <see cref="SemVersion.PrecedenceComparer"/> and
+    /// <see cref="SemVersion.SortOrderComparer"/> so that separate properties aren't needed for the
+    /// <see cref="IEqualityComparer{SemVersion}"/> and <see cref="IComparer{SemVersion}"/>.
+    /// </remarks>
     public interface ISemVersionComparer : IEqualityComparer<SemVersion>, IComparer<SemVersion>, IComparer
     {
     }
