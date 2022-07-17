8c8b9783671168529c5239c2ed2f18e001ab37d2
Jeff Walker <Jeff@WalkerCodeRanger.com>
2022-07-17 12:35:13 -0700

    test SemVersionRange All and AllRelease ranges

--- Semver.Test/Ranges/SemVersionRangeTests.cs
+++ Semver.Test/Ranges/SemVersionRangeTests.cs
@@ -28,16 +28,60 @@ namespace Semver.Test.Ranges
             Assert.False(empty.IncludeAllPrerelease, "Include All Prerelease");
         }
 
+        public static readonly TheoryData<string> FullRangeOfVersions = new TheoryData<string>
+        {
+            "0.0.0",
+            "0.0.0-0",
+            "1.2.3",
+            "4.5.6-rc",
+            "7.8.9-beta.0",
+            "2147483647.2147483647.2147483647-alpha",
+            "2147483647.2147483647.2147483647",
+        };
+
         [Theory]
-        [InlineData("0.0.0")]
-        [InlineData("0.0.0-0")]
-        [InlineData("1.2.3")]
-        [InlineData("4.5.6-rc")]
-        [InlineData("7.8.9-beta.0")]
-        [InlineData("2147483647.2147483647.2147483647")]
+        [MemberData(nameof(FullRangeOfVersions))]
         public void EmptyDoesNotContain(string version)
         {
             Assert.False(SemVersionRange.Empty.Contains(SemVersion.Parse(version, SemVersionStyles.Strict)));
+        }
+
+        [Fact]
+        public void AllReleaseProperties()
+        {
+            var allRelease = SemVersionRange.AllRelease;
+            Assert.Null(allRelease.Start);
+            Assert.False(allRelease.StartInclusive, "Start Inclusive");
+            Assert.Equal(SemVersionRange.MaxVersion, allRelease.End);
+            Assert.True(allRelease.EndInclusive, "End Inclusive");
+            Assert.False(allRelease.IncludeAllPrerelease, "Include All Prerelease");
+        }
+
+        [Theory]
+        [MemberData(nameof(FullRangeOfVersions))]
+        public void AllReleaseContains(string version)
+        {
+            var semVersion = SemVersion.Parse(version, SemVersionStyles.Strict);
+            Assert.Equal(!semVersion.IsPrerelease, SemVersionRange.AllRelease.Contains(semVersion));
+        }
+
+        [Fact]
+        public void AllProperties()
+        {
+            var all = SemVersionRange.All;
+            Assert.Null(all.Start);
+            Assert.False(all.StartInclusive, "Start Inclusive");
+            Assert.Equal(SemVersionRange.MaxVersion, all.End);
+            Assert.True(all.EndInclusive, "End Inclusive");
+            Assert.True(all.IncludeAllPrerelease, "Include All Prerelease");
+        }
+
+        [Theory]
+        [MemberData(nameof(FullRangeOfVersions))]
+        public void AllContains(string version)
+        {
+            var semVersion = SemVersion.Parse(version, SemVersionStyles.Strict);
+            Assert.True(SemVersionRange.All.Contains(semVersion));
         }
     }
 }
