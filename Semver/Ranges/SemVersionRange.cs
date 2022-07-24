fb9d151491c55fa94635628e69385026529f799a
Jeff Walker <Jeff@WalkerCodeRanger.com>
2022-07-23 19:26:49 -0700

    introduce the new SemVersionRange class

---
+++ Semver/Ranges/SemVersionRange.cs
@@ -0,0 +1,11 @@
+﻿namespace Semver.Ranges
+{
+    /// <summary>
+    /// A range of <see cref="SemVersion"/> values. A range can have gaps in it and may include only
+    /// some prerelease versions between included release versions. For a range that cannot have
+    /// gaps see the <see cref="UnbrokenSemVersionRange"/> class.
+    /// </summary>
+    internal class SemVersionRange
+    {
+    }
+}
