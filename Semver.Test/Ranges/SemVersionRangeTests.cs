0ce96d11a5bc0fa4ef0e72194dbb33a56c049c10
Jeff Walker <Jeff@WalkerCodeRanger.com>
2022-07-24 23:39:49 -0700

    basic SemVersionRange construction

---
+++ Semver.Test/Ranges/SemVersionRangeTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Semver.Ranges;
+using Xunit;
+
+namespace Semver.Test.Ranges
+{
+    public class SemVersionRangeTests
+    {
+        [Fact]
+        [SuppressMessage("Assertions", "xUnit2013:Do not use equality check to check for collection size.",
+            Justification = "Need to validate count property.")]
+        public void EmptyContainsNoRanges()
+        {
+            Assert.Equal(0, SemVersionRange.Empty.Count);
+            Assert.Empty(SemVersionRange.Empty);
+        }
+
+        [Fact]
+        public void AllReleaseIsUnbrokenRange()
+        {
+            Assert.Equal(new[] { UnbrokenSemVersionRange.AllRelease }, SemVersionRange.AllRelease);
+        }
+
+        [Fact]
+        public void AllIsUnbrokenRange()
+        {
+            Assert.Equal(new[] { UnbrokenSemVersionRange.All }, SemVersionRange.All);
+        }
+    }
+}
--- Semver/Ranges/SemVersionRange.cs
+++ Semver/Ranges/SemVersionRange.cs
@@ -13,12 +13,56 @@ namespace Semver.Ranges
     /// </summary>
     internal class SemVersionRange : IReadOnlyList<UnbrokenSemVersionRange>
     {
+        public static readonly SemVersionRange Empty = new SemVersionRange(ReadOnlyList<UnbrokenSemVersionRange>.Empty);
+
+        public static readonly SemVersionRange AllRelease = new SemVersionRange(UnbrokenSemVersionRange.AllRelease);
+        public static readonly SemVersionRange All = new SemVersionRange(UnbrokenSemVersionRange.All);
+
+        public static SemVersionRange GreaterThan(SemVersion version, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.GreaterThan(version, includeAllPrerelease));
+
+        public static SemVersionRange AtLeast(SemVersion version, bool includeAllPrerelease = false)
+             => Create(UnbrokenSemVersionRange.AtLeast(version, includeAllPrerelease));
+
+        public static SemVersionRange LessThan(SemVersion version, bool includeAllPrerelease = false)
+             => Create(UnbrokenSemVersionRange.LessThan(version, includeAllPrerelease));
+
+        public static SemVersionRange AtMost(SemVersion version, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.AtMost(version, includeAllPrerelease));
+
+        public static SemVersionRange Inclusive(SemVersion start, SemVersion end, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.Inclusive(start, end, includeAllPrerelease));
+
+        public static SemVersionRange InclusiveOfStart(SemVersion start, SemVersion end, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.InclusiveOfStart(start, end, includeAllPrerelease));
+
+        public static SemVersionRange InclusiveOfEnd(SemVersion start, SemVersion end, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.InclusiveOfEnd(start, end, includeAllPrerelease));
+
+        public static SemVersionRange Exclusive(SemVersion start, SemVersion end, bool includeAllPrerelease = false)
+            => Create(UnbrokenSemVersionRange.Exclusive(start, end, includeAllPrerelease));
+
+        private static SemVersionRange Create(UnbrokenSemVersionRange range)
+            => UnbrokenSemVersionRange.Empty.Equals(range) ? Empty : new SemVersionRange(range);
+
         private readonly IReadOnlyList<UnbrokenSemVersionRange> ranges;
 
         public SemVersionRange(IEnumerable<UnbrokenSemVersionRange> ranges)
-        {
             // TODO order ranges and combine
-            this.ranges = ranges.ToReadOnlyList();
+            : this(ranges.ToReadOnlyList())
+        {
+        }
+
+        public SemVersionRange(UnbrokenSemVersionRange range)
+            : this(new List<UnbrokenSemVersionRange>(1) { range }.AsReadOnly())
+        {
+        }
+
+        /// <remarks>Parameter validation is not performed. The <paramref name="ranges"/> must be
+        /// an immutable list of properly ordered and combined ranges.</remarks>
+        private SemVersionRange(IReadOnlyList<UnbrokenSemVersionRange> ranges)
+        {
+            this.ranges = ranges;
         }
 
         public bool Contains(SemVersion version) => ranges.Any(r => r.Contains(version));
