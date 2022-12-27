8ffa3a3c3d1916b2263e814cbaeb77f75fb2e284
Jeff Walker <Jeff@WalkerCodeRanger.com>
2022-12-27 14:58:34 -0800

    expand SemVersionRangeStandardParsingToStringTests

--- Semver.Test/Ranges/SemVersionRangeStandardParsingToStringTests.cs
+++ Semver.Test/Ranges/SemVersionRangeStandardParsingToStringTests.cs
@@ -16,20 +16,65 @@ namespace Semver.Test.Ranges
     {
         public static readonly TheoryData<RangeParsingToStringTestCase> ParseToStringTestCases = new TheoryData<RangeParsingToStringTestCase>
         {
+            // All and All Release
             Valid("*", "*"),
+            Valid("*.*.*", "*"),
             Valid("*", IncludeAllPrerelease, "*-*"),
             Valid("*-*", "*-*"),
+            Valid("*.*.*-*", "*-*"),
+
+            // Equality
             Valid("1.2.3", "1.2.3"),
+            Valid("1.2.3-rc", "1.2.3-rc"),
+            Valid("1.2.3-rc", IncludeAllPrerelease, "1.2.3-rc"),
+            Valid("=1.2.3", "1.2.3"),
+            Valid("=1.2.3-rc", "1.2.3-rc"),
+            Valid("=1.2.3-rc", IncludeAllPrerelease, "1.2.3-rc"),
+
+            // Wildcard Prerelease
+            Valid("1.2.3-*", "1.2.3-*"),
+            Valid("1.2.3-*", IncludeAllPrerelease, "1.2.3-*"),
+
+            // Wildcard Patch
             Valid("1.2.*", "1.2.*"),
             Valid("1.2.*", IncludeAllPrerelease, "*-* 1.2.*"),
             Valid("1.2.* *-*", "*-* 1.2.*"),
             Valid("1.2.*-*", "1.2.*-*"),
+
+            // Wildcard Minor
             Valid("1.*.*", "1.*"),
-            Valid("1.2.3-*", "1.2.3-*"),
+            Valid("1.*.*", IncludeAllPrerelease, "*-* 1.*"),
+            Valid("1.*.* *-*", "*-* 1.*"),
+            Valid("1.*-*", "1.*-*"),
+
+            // Wildcard after prerelease identifiers
             Valid("1.2.3-rc.*", "1.2.3-rc.*"),
             Valid("1.2.3-5.*", "1.2.3-5.*"),
             Valid("1.2.3-2147483647.*", "1.2.3-2147483647.*"),
             Valid("3.1.4-l.o.n.g.e.r.rc.*", "3.1.4-l.o.n.g.e.r.rc.*"),
+
+            // Tilde Ranges
+            Valid("~2.5.3", "~2.5.3"),
+            Valid("~2.5.3", IncludeAllPrerelease, "*-* ~2.5.3"),
+            Valid("~2.5.3-rc", "~2.5.3-rc"),
+            Valid("~0.5.3", "~0.5.3"),
+            Valid("~0.5.3", IncludeAllPrerelease, "*-* ~0.5.3"),
+            Valid("~0.5.3-rc", "~0.5.3-rc"),
+
+            // Caret Ranges
+            Valid("^6.2.3", "^6.2.3"),
+            Valid("^6.2.3", IncludeAllPrerelease, "*-* ^6.2.3"),
+            Valid("^6.2.3-alpha.1", "^6.2.3-alpha.1"),
+            Valid("^0.0.7", "^0.0.7"),
+            Valid("^0.0.7", IncludeAllPrerelease, "*-* ^0.0.7"),
+            Valid("^0.0.7-now", "^0.0.7-now"),
+
+            // Basic Ranges
+            Valid(">=1.2.3", ">=1.2.3"),
+            Valid("<1.2.3", "<1.2.3"),
+            Valid("<6.0.0 >=1.0.0", ">=1.0.0 <6.0.0"),
+            Valid(">1.0.0 <=3.2.6", ">1.0.0 <=3.2.6"),
+            Valid(">1.0.0 <=3.2.6 || >=1.0.0 <6.0.0", ">=1.0.0 <6.0.0")
         };
 
         [Theory]
