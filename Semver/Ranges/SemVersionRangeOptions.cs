17eb4ce1a5b64bf6638b841b9a47a823bfdd0943
Jeff Walker <Jeff@WalkerCodeRanger.com>
2023-01-15 17:39:22 -0800

    doc comments for SemVersionRangeOptions

--- Semver/Ranges/SemVersionRangeOptions.cs
+++ Semver/Ranges/SemVersionRangeOptions.cs
@@ -2,12 +2,30 @@
 
 namespace Semver.Ranges
 {
+    /// <summary>
+    /// <para>Determines the parsing options and allowed styles of range and version strings passed
+    /// to the <see cref="SemVersionRange.Parse(string,SemVersionRangeOptions,int)"/> and
+    /// <see cref="SemVersionRange.TryParse(string,SemVersionRangeOptions,out SemVersionRange,int)"/>
+    /// methods. These styles only affect which strings are accepted when parsing. The
+    /// constructed ranges and version numbers are valid semantic version ranges without any of the
+    /// optional features in the original string.</para>
+    ///
+    /// <para>Most options only allow additional version styles. However, the
+    /// <see cref="IncludeAllPrerelease"/> option modifies how version ranges are interpreted. With
+    /// this option, all prerelease versions within the range bounds will be considered to be in the
+    /// range. Without this option, only prerelease versions where one comparison with the same
+    /// major, minor, and patch versions is prerelease will satisfy the range. For more information,
+    /// see the <a href="https://semver-nuget.org/ranges/">range expressions documentation</a>.</para>
+    ///
+    /// <para>This enumeration supports a bitwise combination of its member values (e.g.
+    /// <c>SemVersionRangeOptions.OptionalPatch | SemVersionRangeOptions.AllowV</c>).</para>
+    /// </summary>
     [Flags]
     public enum SemVersionRangeOptions
     {
         #region Matching SemVersionStyles
         /// <summary>
-        /// Accept version strings strictly conforming to the SemVer 2.0 spec without metadata.
+        /// Accept versions strictly conforming to the SemVer 2.0 spec without metadata.
         /// </summary>
         Strict = 0,
 
@@ -19,27 +37,27 @@ namespace Semver.Ranges
         AllowLeadingZeros = 1,
 
         /// <summary>
-        /// Allow a leading lowercase "v".
+        /// Allow a leading lowercase "v" on versions.
         /// </summary>
         AllowLowerV = 1 << 3,
 
         /// <summary>
-        /// Allow a leading uppercase "V".
+        /// Allow a leading uppercase "V" on versions.
         /// </summary>
         AllowUpperV = 1 << 4,
 
         /// <summary>
-        /// Allow a leading "v" or "V".
+        /// Allow a leading "v" or "V" on versions.
         /// </summary>
         AllowV = AllowLowerV | AllowUpperV,
 
         /// <summary>
-        /// Patch version number is optional.
+        /// Patch version number is optional on versions.
         /// </summary>
         OptionalPatch = 1 << 5,
 
         /// <summary>
-        /// Minor and patch version numbers are optional.
+        /// Minor and patch version numbers are optional on versions.
         /// </summary>
         OptionalMinorPatch = 1 << 6 | OptionalPatch,
         #endregion
@@ -58,6 +76,11 @@ namespace Semver.Ranges
         AllowMetadata = 1 << 2,
         #endregion
 
+        /// <summary>
+        /// <para>Accept any version string format supported.</para>
+        ///
+        /// <para>The formats accepted by this style will change if/when more formats are supported.</para>
+        /// </summary>
         Loose = AllowLeadingZeros | AllowV | OptionalMinorPatch | AllowMetadata,
     }
 }
