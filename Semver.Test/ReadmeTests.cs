e4ab0f8a1b6acf8ab0b9b810da527f9c1d3ce30e
Jeff Walker <Jeff@WalkerCodeRanger.com>
2023-01-22 17:03:57 -0800

    add tests for validating the readme

---
+++ Semver.Test/ReadmeTests.cs
@@ -0,0 +1,62 @@
+﻿using Semver.Ranges;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Semver.Test
+{
+    /// <summary>
+    /// These tests verify the syntax and output of the examples in the README.md file.
+    /// </summary>
+    public class ReadmeTests
+    {
+        private readonly ITestOutputHelper console;
+
+        public ReadmeTests(ITestOutputHelper console)
+        {
+            this.console = console;
+        }
+
+        [Fact]
+        public void ReadmeRuns()
+        {
+            // Parsing
+            var version = SemVersion.Parse("1.1.0-rc.1+e471d15", SemVersionStyles.Strict);
+
+            // Constructing
+            var v1 = new SemVersion(1, 0);
+            var vNextRc = SemVersion.ParsedFrom(1, 1, 0, "rc.1");
+
+            // Comparing
+            console.WriteLine("# Comparing");
+            if (version.ComparePrecedenceTo(vNextRc) == 0)
+                console.WriteLine($"{version} has the same precedence as {vNextRc}");
+
+            if (version.CompareSortOrderTo(vNextRc) > 0)
+                console.WriteLine($"{version} sorts after {vNextRc}");
+
+            // Manipulating
+            console.WriteLine("# Manipulating");
+            console.WriteLine($"Current: {version}");
+            if (version.IsPrerelease)
+            {
+                console.WriteLine($"Prerelease: {version.Prerelease}");
+                console.WriteLine($"Next release version is: {version.WithoutPrereleaseOrMetadata()}");
+            }
+
+            // Version Ranges
+            console.WriteLine("# Version Ranges");
+            var range = SemVersionRange.Parse("^1.0.0");
+            var prereleaseRange = SemVersionRange.ParseNpm("^1.0.0", includeAllPrerelease: true);
+            console.WriteLine($"Range: {range}");
+            console.WriteLine($"Prerelease range: {prereleaseRange}");
+            console.WriteLine($"Range includes version: {range.Contains(version)}");
+            console.WriteLine($"Prerelease range includes version: {prereleaseRange.Contains(version)}");
+
+            // Alternative: another way to call SemVersionRange.Contains(version)
+            Assert.False(version.Satisfies(range));
+
+            // Alternative: slower because it parses the range on every call
+            Assert.True(version.Satisfies("^1.0.0", SemVersionRangeOptions.IncludeAllPrerelease));
+        }
+    }
+}
